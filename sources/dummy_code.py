5e140b8d94663a0e33dc9bdda8da1f1d8a20cbf1
Michael <msuttles@gmail.com>
2019-08-26 16:06:52 -0400

    Added description to dummy code

--- sources/dummy_code.py
+++ sources/dummy_code.py
@@ -16,17 +16,32 @@ def distance_between_shelters(shelter_a, shelter_b):
 
  NOTE: if there is a small sample size at a shelter, we need to take this into account. We don't want to be drawing conclusions based on 1 animal. Could even do some sort of automate p-value or something?
 """
+def analyze(shelter):
+    '''
+    Perform an analysis on the shelter to see which animals are most likely to be adopted and which ones are least likely. 
+    Analyze based on:
+    - age
+    - size
+    - appearance
+    - gender?
+    Save the analysis somewhere--perhaps as part of the shelter object?
+    '''
+    pass
 
 # The fun part!
 def recommend(animals, shelters):
     '''
     'animals' is a list of animal objects, and 'shelters' is a list of shelter objects.
     Returns a dict with the keys as the shelters and the values as the animals to be transferred to each shelter
+
+    We also need a mechanism to specify how many animals we are allowed to transfer in to each shelter. Suggestions accepted on the best way to do this?
     '''
     results = {}
     for a in animals:
         pass
     return results
+
+
 
 # Take into consideration how old the data is; make the older data less relevant by multiplying it by a factor of 0.3 or something--or a gradient that progressively makes older data less relevant?
 
